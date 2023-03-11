22d111cd54c3386cf997ccf837d613e6036ad8de
KexinWWW <kexinwang2023@u.northwestern.edu>
2023-03-11 15:12:11 +0000

    commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,18 +6,22 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
   end
 
   def new
     @place = Place.new
   end
 
+  
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
+    if @current_user
+      @place = Place.new
+      @place["name"] = params["place"]["name"]
+      @place.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places"
   end
-
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,30 @@
 class SessionsController < ApplicationController
   def new
   end
-
+  
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Nope. Please try again."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope.Please try again."
+      redirect_to "/login"
+    end
+
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
+  
 end
   
